8b4e0893809efb56ca551eca5c9207193c8f86bf
Vinta <vinta.chen@gmail.com>
2017-05-14 03:33:20 +0800

    no more SQLite

--- albedo/settings.py
+++ albedo/settings.py
@@ -77,13 +77,6 @@ WSGI_APPLICATION = 'albedo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-DATABASES = {
-    'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'albedo',
         'USER': 'root',
