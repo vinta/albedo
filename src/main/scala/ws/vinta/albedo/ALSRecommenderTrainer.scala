28f33ea46a375616ae4a8834f20ec48ad8b1d7e5
Vinta <vinta.chen@gmail.com>
2017-09-03 00:27:11 +0800

    train a als model

--- src/main/scala/ws/vinta/albedo/ALSRecommenderTrainer.scala
+++ src/main/scala/ws/vinta/albedo/ALSRecommenderTrainer.scala
@@ -1,6 +1,7 @@
 package ws.vinta.albedo
 
-import org.apache.spark.ml.recommendation.ALS
+import org.apache.hadoop.mapred.InvalidInputException
+import org.apache.spark.ml.recommendation.{ALS, ALSModel}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.collect_list
 import ws.vinta.albedo.evaluators.RankingEvaluator
@@ -19,23 +20,41 @@ object ALSRecommenderTrainer {
 
     val sc = spark.sparkContext
 
+    // Load Data
+
     val rawRepoStarringDS = loadRepoStarring()
     rawRepoStarringDS.cache()
     rawRepoStarringDS.printSchema()
 
-    val als = new ALS()
-      .setImplicitPrefs(true)
-      .setRank(50)
-      .setRegParam(0.5)
-      .setAlpha(40)
-      .setMaxIter(20)
-      .setSeed(42)
-      .setColdStartStrategy("drop")
-      .setUserCol("user_id")
-      .setItemCol("repo_id")
-      .setRatingCol("starring")
-    val alsModel = als.fit(rawRepoStarringDS)
-    alsModel.save(s"${Settings.dataDir}/${Settings.today}/alsModel.parquet")
+    // Train Model
+
+    val alsModelSavePath = s"${Settings.dataDir}/${Settings.today}/alsModel.parquet"
+    val alsModel: ALSModel = try {
+      ALSModel.load(alsModelSavePath)
+    } catch {
+      case e: InvalidInputException => {
+        if (e.getMessage().contains("Input path does not exist")) {
+          val als = new ALS()
+            .setImplicitPrefs(true)
+            .setRank(100)
+            .setRegParam(0.5)
+            .setAlpha(40)
+            .setMaxIter(22)
+            .setSeed(42)
+            .setColdStartStrategy("drop")
+            .setUserCol("user_id")
+            .setItemCol("repo_id")
+            .setRatingCol("starring")
+          val alsModel = als.fit(rawRepoStarringDS)
+          alsModel.save(alsModelSavePath)
+          alsModel
+        } else {
+          throw e
+        }
+      }
+    }
+
+    // Make Recommendations
 
     val k = 15
 
@@ -44,9 +63,13 @@ object ALSRecommenderTrainer {
 
     val userRecommendationsDS = userRecommendationsDF.as[UserRecommendations]
 
+    // Evaluate Model
+
     val userActualItemsDF = rawRepoStarringDS
+      .orderBy($"starred_at".desc)
       .groupBy($"user_id")
       .agg(collect_list($"repo_id").alias("items"))
+    
     val rankingEvaluator = new RankingEvaluator(userActualItemsDF)
       .setMetricName("ndcg@k")
       .setK(k)
