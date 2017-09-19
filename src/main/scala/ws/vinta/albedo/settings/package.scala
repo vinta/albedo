a23da0a27975b1be8ca4e8507935774cca8efd05
Vinta <vinta.chen@gmail.com>
2017-09-19 16:16:07 +0800

    rename

--- src/main/scala/ws/vinta/albedo/settings/package.scala
+++ src/main/scala/ws/vinta/albedo/settings/package.scala
@@ -7,9 +7,12 @@ package object settings {
   private val sc = spark.sparkContext
 
   val dataDir: String = sc.getConf.get("spark.albedo.dataDir", "./spark-data")
+  //val dataDir: String = "/mnt/albedo_s3/spark-data"
 
   def today: String = {
     val dateFormatter = new java.text.SimpleDateFormat("yyyyMMdd")
     dateFormatter.format(new java.util.Date())
   }
+
+  val emptyStringPlaceholder = "__empty"
 }
