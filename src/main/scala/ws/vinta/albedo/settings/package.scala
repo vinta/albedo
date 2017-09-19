426f44daaaaf91cd8ef3c45fe20fe602df1d9727
Vinta <vinta.chen@gmail.com>
2017-09-20 03:59:07 +0800

    refine

--- src/main/scala/ws/vinta/albedo/settings/package.scala
+++ src/main/scala/ws/vinta/albedo/settings/package.scala
@@ -7,12 +7,10 @@ package object settings {
   private val sc = spark.sparkContext
 
   val dataDir: String = sc.getConf.get("spark.albedo.dataDir", "./spark-data")
-  //val dataDir: String = "/mnt/albedo_s3/spark-data"
+  //val dataDir: String = sc.getConf.get("spark.albedo.dataDir", "/mnt/albedo_s3/spark-data")
 
   def today: String = {
     val dateFormatter = new java.text.SimpleDateFormat("yyyyMMdd")
     dateFormatter.format(new java.util.Date())
   }
-
-  val emptyStringPlaceholder = "__empty"
 }
