56fb850561e0391777f4919273d94eeaf65fabcf
Vinta <vinta.chen@gmail.com>
2017-10-07 19:13:02 +0800

    create CurationRecommender

--- src/main/scala/ws/vinta/albedo/CurationRecommenderBuilder.scala
+++ src/main/scala/ws/vinta/albedo/CurationRecommenderBuilder.scala
@@ -1,9 +1,9 @@
 package ws.vinta.albedo
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions._
 import ws.vinta.albedo.evaluators.RankingEvaluator
 import ws.vinta.albedo.evaluators.RankingEvaluator._
+import ws.vinta.albedo.recommenders.CurationRecommender
 import ws.vinta.albedo.schemas._
 import ws.vinta.albedo.utils.DatasetUtils._
 
@@ -11,7 +11,7 @@ object CurationRecommenderBuilder {
   def main(args: Array[String]): Unit = {
     implicit val spark: SparkSession = SparkSession
       .builder()
-      .appName("CurationRecommender")
+      .appName("CurationRecommenderBuilder")
       .getOrCreate()
 
     import spark.implicits._
@@ -21,32 +21,32 @@ object CurationRecommenderBuilder {
     val rawStarringDS = loadRawStarringDS()
     rawStarringDS.cache()
 
-    val rawUserInfoDS = loadRawUserInfoDS()
-    rawUserInfoDS.cache()
+    // Split Data
 
-    // 統一使用 20% 的 test set 來評估每個演算法
+    // 雖然不是每個演算法都需要劃分 training set 和 test set
+    // 不過為了方便比較，我們還是統一使用 20% 的 test set 來評估每個模型
     val Array(_, testDF) = rawStarringDS.randomSplit(Array(0.8, 0.2))
     testDF.cache()
 
-    val testUserDF = testDF.select($"user_id").distinct()
+    val meDF = spark.createDataFrame(Seq(
+      (652070, "vinta")
+    )).toDF("user_id", "username")
+
+    val testUserDF = testDF.select($"user_id").union(meDF.select($"user_id")).distinct()
 
     // Make Recommendations
 
     val topK = 30
 
-    val curatorIds = Array(652070, 1912583, 59990, 646843, 28702) // vinta, saiday, tzangms, fukuball, wancw
-    val curatedRepoDF = rawStarringDS
-      .select($"repo_id", $"starred_at")
-      .where($"user_id".isin(curatorIds: _*))
-      .groupBy($"repo_id")
-      .agg(max($"starred_at").alias("starred_at"))
-      .orderBy($"starred_at".desc)
-      .limit(topK)
-    curatedRepoDF.cache()
+    val curationRecommender = new CurationRecommender()
+      .setUserCol("user_id")
+      .setItemCol("repo_id")
+      .setTopK(topK)
 
-    val userCuratedRepoDF = rawUserInfoDS
-      .select($"user_id")
-      .crossJoin(curatedRepoDF)
+    val userRecommendedItemDF = curationRecommender.recommendForUsers(testUserDF)
+    userRecommendedItemDF.cache()
+
+    userRecommendedItemDF.where($"user_id" === 652070).show(false)
 
     // Evaluate the Model
 
@@ -54,9 +54,9 @@ object CurationRecommenderBuilder {
       .transform(intoUserActualItems($"user_id", $"repo_id", $"starred_at".desc, topK))
       .as[UserItems]
 
-    val userPredictedItemsDS = userCuratedRepoDF
+    val userPredictedItemsDS = userRecommendedItemDF
       .join(testUserDF, Seq("user_id"))
-      .transform(intoUserPredictedItems($"user_id", $"repo_id", $"starred_at".desc, topK))
+      .transform(intoUserPredictedItems($"user_id", $"repo_id", $"score".desc, topK))
       .as[UserItems]
 
     val rankingEvaluator = new RankingEvaluator(userActualItemsDS)
@@ -66,7 +66,7 @@ object CurationRecommenderBuilder {
       .setItemsCol("items")
     val metric = rankingEvaluator.evaluate(userPredictedItemsDS)
     println(s"${rankingEvaluator.getMetricName} = $metric")
-    // NDCG@k = 0.000259959
+    // NDCG@k = 0.00067849479
 
     spark.stop()
   }
---
+++ src/main/scala/ws/vinta/albedo/recommenders/CurationRecommender.scala
@@ -0,0 +1,45 @@
+package ws.vinta.albedo.recommenders
+
+import org.apache.spark.ml.util.Identifiable
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import ws.vinta.albedo.utils.DatasetUtils._
+
+class CurationRecommender(override val uid: String) extends Recommender {
+
+  def this() = {
+    this(Identifiable.randomUID("curationRecommender"))
+  }
+
+  override def source = "curation"
+
+  override def recommendForUsers(userDF: Dataset[_]): DataFrame = {
+    transformSchema(userDF.schema)
+
+    implicit val spark: SparkSession = userDF.sparkSession
+    import spark.implicits._
+
+    val rawStarringDS = loadRawStarringDS()
+    rawStarringDS.cache()
+
+    val curatorIds = Array(652070, 1912583, 59990, 646843, 28702) // vinta, saiday, tzangms, fukuball, wancw
+    val curatedRepoDF = rawStarringDS
+      .select($"repo_id", $"starred_at")
+      .where($"user_id".isin(curatorIds: _*))
+      .groupBy($"repo_id")
+      .agg(max($"starred_at").alias("starred_at"))
+      .orderBy($"starred_at".desc)
+      .limit($(topK))
+    curatedRepoDF.cache()
+
+    def calculateScoreUDF = udf((starred_at: java.sql.Timestamp) => {
+      starred_at.getTime / 1000.0
+    })
+
+    userDF
+      .select($(userCol))
+      .crossJoin(curatedRepoDF)
+      .select(col($(userCol)), $"repo_id".alias($(itemCol)), calculateScoreUDF($"starred_at").alias($(scoreCol)))
+      .withColumn($(sourceCol), lit(source))
+  }
+}
