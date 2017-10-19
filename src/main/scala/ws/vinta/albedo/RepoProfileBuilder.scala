a24b2712d974e1daad3e94563e403d2c7b342293
Vinta <vinta.chen@gmail.com>
2017-10-19 15:08:54 +0800

    improve repo profile builder

--- src/main/scala/ws/vinta/albedo/RepoProfileBuilder.scala
+++ src/main/scala/ws/vinta/albedo/RepoProfileBuilder.scala
@@ -37,14 +37,14 @@ object RepoProfileBuilder {
 
     val continuousColumnNames = mutable.ArrayBuffer.empty[String]
     val categoricalColumnNames = mutable.ArrayBuffer.empty[String]
+    val listColumnNames = mutable.ArrayBuffer.empty[String]
     val textColumnNames = mutable.ArrayBuffer.empty[String]
 
+    continuousColumnNames += "size"
     continuousColumnNames += "stargazers_count"
     continuousColumnNames += "forks_count"
     continuousColumnNames += "subscribers_count"
     continuousColumnNames += "open_issues_count"
-
-    // TODO: listColumnNames: topics
 
     // Impute Data
 
@@ -90,21 +90,19 @@ object RepoProfileBuilder {
       .withColumn("is_unmaintained", when(unmaintainedWords.map($"clean_description".like(_)).reduce(_ or _), 1.0).otherwise(0.0))
       .withColumn("is_assignment", when(assignmentWords.map($"clean_description".like(_)).reduce(_ or _), 1.0).otherwise(0.0))
       .where($"is_unmaintained" === 0 and $"is_assignment" === 0)
-      .drop($"is_unmaintained")
-      .drop($"is_assignment")
       .withColumn("days_between_created_at_today", datediff(current_date(), $"created_at"))
       .withColumn("days_between_updated_at_today", datediff(current_date(), $"updated_at"))
       .withColumn("days_between_pushed_at_today", datediff(current_date(), $"pushed_at"))
-      .withColumn("watch_star_count_ratio", round($"subscribers_count" / ($"stargazers_count" + lit(1)), 3))
-      .withColumn("fork_star_count_ratio", round($"forks_count" / ($"stargazers_count" + lit(1)), 3))
+      .withColumn("stargazers_subscribers_count_ratio", round($"stargazers_count" / ($"subscribers_count" + lit(1)), 3))
+      .withColumn("stargazers_forks_count_ratio", round($"stargazers_count" / ($"forks_count" + lit(1)), 3))
       .withColumn("is_vinta_starred", when($"repo_id".isin(vintaStarredRepos: _*), 1.0).otherwise(0.0))
-      .withColumn("text", concat_ws(" ", $"owner_username", $"name", $"language", $"description", $"topics"))
+      .withColumn("text", concat_ws(" ", $"owner_username", $"name", $"language", $"description"))
 
     continuousColumnNames += "days_between_created_at_today"
     continuousColumnNames += "days_between_updated_at_today"
     continuousColumnNames += "days_between_pushed_at_today"
-    continuousColumnNames += "watch_star_count_ratio"
-    continuousColumnNames += "fork_star_count_ratio"
+    continuousColumnNames += "stargazers_subscribers_count_ratio"
+    continuousColumnNames += "stargazers_forks_count_ratio"
 
     categoricalColumnNames += "is_vinta_starred"
 
@@ -120,18 +118,26 @@ object RepoProfileBuilder {
       .join(languagesDF, Seq("clean_language"))
       .withColumn("has_homepage", when($"homepage" === "", 0.0).otherwise(1.0))
       .withColumn("binned_language", when($"count_per_language" <= 30, "__other").otherwise($"clean_language"))
+      .withColumn("clean_topics", split($"topics", ","))
     transformedRepoInfoDF.cache()
 
     categoricalColumnNames += "has_homepage"
     categoricalColumnNames += "binned_language"
 
+    listColumnNames += "clean_topics"
+
     // Save Results
 
+    // Continuous column names: size, stargazers_count, forks_count, subscribers_count, open_issues_count, days_between_created_at_today, days_between_updated_at_today, days_between_pushed_at_today, stargazers_subscribers_count_ratio, stargazers_forks_count_ratio
+    // Categorical column names: owner_type, clean_has_issues, clean_has_projects, clean_has_downloads, clean_has_wiki, clean_has_pages, is_vinta_starred, has_homepage, binned_language
+    // List column names: clean_topics
+    // Text column names: text
     println("Continuous column names: " + continuousColumnNames.mkString(", "))
     println("Categorical column names: " + categoricalColumnNames.mkString(", "))
+    println("List column names: " + listColumnNames.mkString(", "))
     println("Text column names: " + textColumnNames.mkString(", "))
 
-    val featureNames = mutable.ArrayBuffer("repo_id", "full_name") ++ continuousColumnNames ++ categoricalColumnNames ++ textColumnNames
+    val featureNames = mutable.ArrayBuffer("repo_id", "full_name", "owner_id") ++ continuousColumnNames ++ categoricalColumnNames ++ listColumnNames ++ textColumnNames
     val path = s"${settings.dataDir}/${settings.today}/repoProfileDF.parquet"
     val repoProfileDF = loadOrCreateDataFrame(path, () => {
       transformedRepoInfoDF.select(featureNames.map(col): _*)
