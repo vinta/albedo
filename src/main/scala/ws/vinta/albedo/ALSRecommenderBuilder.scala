62a56aa7318d6abf7e0cdf4dd06f14339bc1c46e
Vinta <vinta.chen@gmail.com>
2017-10-27 02:37:47 +0800

    RUN_WITH_INTELLIJ

--- src/main/scala/ws/vinta/albedo/ALSRecommenderBuilder.scala
+++ src/main/scala/ws/vinta/albedo/ALSRecommenderBuilder.scala
@@ -13,11 +13,11 @@ import ws.vinta.albedo.recommenders.ALSRecommender
 object ALSRecommenderBuilder {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf()
-    if (scala.util.Properties.envOrElse("RUN_ON_SMALL_MACHINE", "false") == "true") {
-      //conf.setMaster("local[*]")
-      //conf.set("spark.driver.memory", "12g")
-      conf.setMaster("local-cluster[1, 3, 12288]")
-      conf.setJars(List("target/albedo-1.0.0-SNAPSHOT-uber.jar"))
+    if (scala.util.Properties.envOrElse("RUN_WITH_INTELLIJ", "false") == "true") {
+      conf.setMaster("local[*]")
+      conf.set("spark.driver.memory", "12g")
+      //conf.setMaster("local-cluster[1, 3, 12288]")
+      //conf.setJars(List("target/albedo-1.0.0-SNAPSHOT-uber.jar"))
       //conf.setMaster("spark://localhost:7077")
       //conf.set("spark.driver.memory", "2g")
       //conf.set("spark.executor.cores", "3")
--- src/main/scala/ws/vinta/albedo/UserProfileBuilder.scala
+++ src/main/scala/ws/vinta/albedo/UserProfileBuilder.scala
@@ -12,9 +12,11 @@ import scala.collection.mutable
 object UserProfileBuilder {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf()
-    if (scala.util.Properties.envOrElse("RUN_ON_SMALL_MACHINE", "false") == "true") {
+    if (scala.util.Properties.envOrElse("RUN_WITH_INTELLIJ", "false") == "true") {
       conf.setMaster("local[*]")
       conf.set("spark.driver.memory", "12g")
+      //conf.setMaster("local-cluster[1, 3, 12288]")
+      //conf.setJars(List("target/albedo-1.0.0-SNAPSHOT-uber.jar"))
       //conf.setMaster("spark://localhost:7077")
       //conf.set("spark.driver.memory", "2g")
       //conf.set("spark.executor.cores", "3")
